058023a4acc4a9108d66cd401f84e24b70dd86b3
Wahyu Setiawan <whysetiawan27@gmail.com>
2023-05-26 18:14:52 +0700

    faker

---
+++ src/features/generateFile.js
@@ -0,0 +1,17 @@
+import faker from "@faker-js/faker";
+
+const products = [
+  {
+    category: "Tas",
+    price: 100,
+    brands: [
+      { name: "Erger", price: 20 },
+      { name: "Ahdadas", price: 20 },
+      { name: "JinSprot", price: 5 },
+      { name: "Erger", price: 20 },
+      { name: "Erger", price: 20 },
+    ],
+  },
+];
+
+const generateData = ({ ammount = 100 } = {}) => {};
